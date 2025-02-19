a3aa11c137c551a37b643e0aeb98f967401f8c41
David Eliason <davethemaker@gmail.com>
2025-02-18 20:20:17 -0800

    add resource tag default for vpc

--- back_end/resource_code/terraform/variables.tf
+++ back_end/resource_code/terraform/variables.tf
@@ -3,3 +3,11 @@ variable "cidr_block" {
   type        = string
   default     = "10.16.0.0/16"
 }
+
+variable "resource_tags" {
+  description = "The tags for the resources"
+  type        = map(string)
+  default = {
+    Name = "main-vpc"
+  }
+}
