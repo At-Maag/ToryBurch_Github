44aeced86b3e9f5c3eadcdbd9e4e9818e1ad3ca4
David Eliason <davethemaker@gmail.com>
2025-02-23 10:08:22 -0800

    refactor vpc from resource to module structure for modularity

--- back_end/resource_code/terraform/main.tf
+++ back_end/resource_code/terraform/main.tf
@@ -1,11 +1,15 @@
 # add Terraform and provider
 
-resource "aws_vpc" "main-vpc" {
+module "main-vpc" {
+  source               = "./modules/vpc"
   cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags                 = var.resource_tags
 }
+
+
+
 
 resource "aws_internet_gateway" "main-igw" {
   vpc_id = aws_vpc.main-vpc.id # this value will be the id of the vpc created above
