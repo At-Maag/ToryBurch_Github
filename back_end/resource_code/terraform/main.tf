c6ec0703bed9d64a72dd6a085d2912cf8451c378
David Eliason <davethemaker@gmail.com>
2025-02-23 14:14:04 -0800

    refactor as previous config did not work. One resource at a time, starting with vpc

--- back_end/resource_code/terraform/main.tf
+++ back_end/resource_code/terraform/main.tf
@@ -17,21 +17,10 @@ provider "aws" {
   region                   = "us-west-2"
 }
 
-resource "aws_vpc" "main-vpc" {
-  cidr_block = var.vpc_cidr_block
-  tags       = var.resource_tags
+module "vpc" {
+  source     = "./modules/vpc"
+  cidr_block = "10.16.0.0/16"
+
 }
 
 
-resource "aws_internet_gateway" "main-igw" {
-  vpc_id = aws_vpc.main-vpc.id # this value will be the id of the vpc created above
-  tags   = var.resource_tags
-}
-
-resource "aws_subnet" "pub-subnet-1" {
-  vpc_id                  = aws_vpc.main-vpc.id # reference the vpc created above
-  cidr_block              = var.pub_subnet_cidr_blocks[0]
-  availability_zone       = var.availability_zones[0]
-  map_public_ip_on_launch = true
-  tags                    = var.resource_tags
-}
--- back_end/resource_code/terraform/variables.tf
+++ back_end/resource_code/terraform/variables.tf
@@ -1,22 +1,6 @@
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-  type        = string
-  default     = "10.16.0.0/16"
-}
 
-variable "resource_tags" {
-  description = "The tags for the resources"
-  type        = map(string)
-  default = {
-    Name = "main-vpc"
-  }
-}
 
-variable "pub_subnet_cidr_blocks" {
-  description = "The CIDR blocks for the public subnets"
-  type        = list(string)
-  default     = ["10.16.0.0/20", "10.16.16.0/20"]
-}
+
 
 variable "availability_zones" {
   description = "The availability zones for the subnets"
