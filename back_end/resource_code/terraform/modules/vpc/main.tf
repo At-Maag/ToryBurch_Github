3da40184a595f24d9f7372d6785aaa30ffeed0e4
David Eliason <davethemaker@gmail.com>
2025-02-25 18:36:43 -0800

    create default route pointing to the IGW within the public RT

--- back_end/resource_code/terraform/modules/vpc/main.tf
+++ back_end/resource_code/terraform/modules/vpc/main.tf
@@ -43,6 +43,10 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main-vpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
   #add custom tags
   tags = merge(var.tags, {
     Name = "ToryBurch-public-route-table"
