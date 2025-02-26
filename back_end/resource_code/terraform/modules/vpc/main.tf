0693ca52385029ea7620ede3bebc10f209b18742
David Eliason <davethemaker@gmail.com>
2025-02-25 18:32:40 -0800

    add public subnet association with public route table

--- back_end/resource_code/terraform/modules/vpc/main.tf
+++ back_end/resource_code/terraform/modules/vpc/main.tf
@@ -49,4 +49,9 @@ resource "aws_route_table" "public-route-table" {
   })
 }
 
+#associate public subnet with public route table
+resource "aws_route_table_association" "public-subnet-association" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-route-table.id
+}
 
